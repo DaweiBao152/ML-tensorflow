21e29e49093c97c1c6b3fa8622af11a0b89a60c9
baodawei <14561456+david-bao@user.noreply.gitee.com>
2024-07-19 14:23:44 +0800

    add test file to test model including learning graph

---
+++ model_tester.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import json
+import tensorflow as tf
+import numpy as np
+
+
+# load the model that has been packed
+def load_model():
+    c = np.load('test_X.npy')
+    d = np.load('test_y.npy')
+    return c, d
+
+
+test_images, test_labels = load_model()
+
+# using following code to load model
+loaded_model = tf.keras.models.load_model('my_model.h5')
+
+test_loss, test_acc = loaded_model.evaluate(test_images, test_labels, verbose=2)
+print('Test accuracy: ', test_acc)
+
+# load history from file
+with open('history.json', 'r') as f:
+    history_data = json.load(f)
+
+
+# recreate history object to draw the graph
+class History:
+    def __init__(self, history):
+        self.history = history
+
+
+history = History(history_data)
+
+# draw the learning curve
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(10, 6))
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.title('Learning Curves')
+plt.legend()
+plt.show()
